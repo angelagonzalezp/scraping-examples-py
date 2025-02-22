52a524843d747367ce17fac25f9f284db245044a
angelagonzalezp <angelaglezp@gmail.com>
2025-02-22 13:43:56 +0100

    Upload script to scrape goodreads Top 100 books

---
+++ goodreads/src/constants.py
@@ -0,0 +1,7 @@
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
+GOODREADS_TOP_URL = "https://www.goodreads.com/list/show/153860.Goodreads_Top_100_Highest_Rated_Books_on_Goodreads_with_at_least_10_000_Ratings"
+TOP_TABLE_DIV_ID = "all_votes"
+BOOK_TITLE_CLASS = "bookTitle"
+AUTHOR_NAME_CLASS = "authorName"
+RATING_CLASS = "minirating"
+BASE_URL = "https://www.goodreads.com"
