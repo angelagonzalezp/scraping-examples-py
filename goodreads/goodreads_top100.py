c00569c4dbf6c56214050e78e2db26f92199ea87
Angela GP <71433272+angelagonzalezp@users.noreply.github.com>
2025-08-13 18:47:59 +0200

    Improve goodreads script so that we can scrape any list (#5)

--- goodreads/goodreads_top100.py
+++ goodreads/goodreads_top100.py
@@ -6,8 +6,8 @@ from src.constants import *
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def main(filename, upload_to_mongo=False):
-    html = get_html(GOODREADS_TOP_URL)
+def main(filename, url, upload_to_mongo=False):
+    html = get_html(url)
     top_div = get_top_table_element(html)
     top_books_data = scrape_table_data(top_div)
     top_books_df = scraped_dict_to_csv(top_books_data, filename)
@@ -19,6 +19,7 @@ if __name__ == "__main__":
                     prog="goodreads_top100",
                     description="This program scrapes goodreads Top 100.")
     parser.add_argument("-o", "--output", required=True)
+    parser.add_argument("-u", "--url", required=False, default=GOODREADS_TOP_URL)
     parser.add_argument("-m", "--mongo", required=False, action='store_true')
     args = parser.parse_args()
-    main(args.output, args.mongo)
+    main(args.output, args.url, args.mongo)
